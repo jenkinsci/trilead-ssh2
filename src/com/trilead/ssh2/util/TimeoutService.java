a528acf3709eb8100db3d9f0a8925fcec3a2d46e
Stefan Zier <stefan@sumologic.com>
2015-04-29 17:58:58 -0700

    Making the threads Daemon threads.

--- src/com/trilead/ssh2/util/TimeoutService.java
+++ src/com/trilead/ssh2/util/TimeoutService.java
@@ -3,59 +3,74 @@ package com.trilead.ssh2.util;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
  * TimeoutService (beta). Here you can register a timeout.
- * <p>
+ * <p/>
  * Implemented having large scale programs in mind: if you open many concurrent SSH connections
  * that rely on timeouts, then there will be only one timeout thread. Once all timeouts
  * have expired/are cancelled, the thread will (sooner or later) exit.
  * Only after new timeouts arrive a new thread (singleton) will be instantiated.
- * 
+ *
  * @author Christian Plattner, plattner@trilead.com
  * @version $Id: TimeoutService.java,v 1.1 2007/10/15 12:49:57 cplattne Exp $
  */
 public class TimeoutService {
 
-	private final static ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(20);
+    private final static ThreadFactory threadFactory = new ThreadFactory() {
 
-	public static class TimeoutToken implements Runnable {
-		private Runnable handler;
-		private boolean cancelled = false;
+        private AtomicInteger count = new AtomicInteger();
 
-		public void run() {
-			if (!cancelled) {
-				handler.run();
-			}
-		}
-	}
+        public Thread newThread(Runnable r) {
+            int threadNumber = count.incrementAndGet();
+            String threadName = "TimeoutService-" + threadNumber;
+            Thread thread = new Thread(r, threadName);
+            thread.setDaemon(true);
+            return thread;
+        }
+    };
 
-	/**
-	 * It is assumed that the passed handler will not execute for a long time.
-	 * 
-	 * @param runTime
-	 * @param handler
-	 * @return a TimeoutToken that can be used to cancel the timeout.
-	 */
-	public static final TimeoutToken addTimeoutHandler(long runTime, Runnable handler) {
-		TimeoutToken token = new TimeoutToken();
-		token.handler = handler;
-		long delay = runTime - System.currentTimeMillis();
-		if (delay < 0) {
-			delay = 0;
-		}
-		scheduler.schedule(token, delay, TimeUnit.MILLISECONDS);
-		return token;
-	}
+    private final static ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(20, threadFactory);
 
-	/**
-	 * Cancel the timeout callback for the specified token.
-	 *
-	 * @param token
-	 */
-	public static final void cancelTimeoutHandler(TimeoutToken token) {
-		token.cancelled = true;
-	}
+    public static class TimeoutToken implements Runnable {
+        private Runnable handler;
+        private boolean cancelled = false;
+
+        public void run() {
+            if (!cancelled) {
+                handler.run();
+            }
+        }
+    }
+
+    /**
+     * It is assumed that the passed handler will not execute for a long time.
+     *
+     * @param runTime
+     * @param handler
+     * @return a TimeoutToken that can be used to cancel the timeout.
+     */
+    public static final TimeoutToken addTimeoutHandler(long runTime, Runnable handler) {
+        TimeoutToken token = new TimeoutToken();
+        token.handler = handler;
+        long delay = runTime - System.currentTimeMillis();
+        if (delay < 0) {
+            delay = 0;
+        }
+        scheduler.schedule(token, delay, TimeUnit.MILLISECONDS);
+        return token;
+    }
+
+    /**
+     * Cancel the timeout callback for the specified token.
+     *
+     * @param token
+     */
+    public static final void cancelTimeoutHandler(TimeoutToken token) {
+        token.cancelled = true;
+    }
 }
