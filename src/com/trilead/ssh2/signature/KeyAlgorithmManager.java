c2962b44489982fcfbe8bebeb5969195b234a698
Michael Clarke <michael.m.clarke@gmail.com>
2017-03-31 18:46:46 +0100

    Add support for ECDSA keys

--- src/com/trilead/ssh2/signature/KeyAlgorithmManager.java
+++ src/com/trilead/ssh2/signature/KeyAlgorithmManager.java
@@ -1,5 +1,7 @@
 package com.trilead.ssh2.signature;
 
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.ArrayList;
@@ -26,6 +28,16 @@ public final class KeyAlgorithmManager {
     private static Collection<KeyAlgorithm<PublicKey, PrivateKey>> buildSupportAlgorithmsList() {
         List<KeyAlgorithm<?, ?>> algorithms = new ArrayList<>();
         algorithms.add(new ED25519KeyAlgorithm());
+
+        try {
+            KeyFactory.getInstance("EC");
+            algorithms.add(new ECDSAKeyAlgorithm.ECDSASha2Nistp521());
+            algorithms.add(new ECDSAKeyAlgorithm.ECDSASha2Nistp384());
+            algorithms.add(new ECDSAKeyAlgorithm.ECDSASha2Nistp256());
+        } catch (GeneralSecurityException ex) {
+            // we don't use ECDSA algorithms in this case
+        }
+
 
         algorithms.add(new RSAKeyAlgorithm());
         algorithms.add(new DSAKeyAlgorithm());
