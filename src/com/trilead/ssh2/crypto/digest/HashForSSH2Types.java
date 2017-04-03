f248557bc9cc8e3f6534de0ea0ba4607e7d8fd0d
paladox <paladox@users.noreply.github.com>
2017-04-03 10:56:30 +0100

    Update HashForSSH2Types.java

--- src/com/trilead/ssh2/crypto/digest/HashForSSH2Types.java
+++ src/com/trilead/ssh2/crypto/digest/HashForSSH2Types.java
@@ -8,9 +8,8 @@ import java.math.BigInteger;
  * 
  * @author Christian Plattner, plattner@trilead.com
  * @version $Id: HashForSSH2Types.java,v 1.1 2007/10/15 12:49:57 cplattne Exp $
- *
- * @deprecated use HashForSSH2TypesNew instead
  */
+// @deprecated use HashForSSH2TypesNew instead
 public class HashForSSH2Types instead
 {
 	Digest md;
