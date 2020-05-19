43eb37b0a63c44411a4686f96a5d22358871a142
Matt Sicker <boards@gmail.com>
2020-05-19 16:03:13 -0500

    [JENKINS-62552] Use standard crypto APIs
    This deprecates the included implementations of block ciphers in preference of the ones bundled with the JDK.
    
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- src/com/trilead/ssh2/crypto/cipher/BlockCipherFactory.java
+++ src/com/trilead/ssh2/crypto/cipher/BlockCipherFactory.java
@@ -1,6 +1,7 @@
 
 package com.trilead.ssh2.crypto.cipher;
 
+import javax.crypto.spec.IvParameterSpec;
 import java.util.Vector;
 
 /**
@@ -14,46 +15,46 @@ public class BlockCipherFactory
 	static class CipherEntry
 	{
 		String type;
+		String algorithm;
 		int blocksize;
 		int keysize;
-		String cipherClass;
 
-		public CipherEntry(String type, int blockSize, int keySize, String cipherClass)
+		public CipherEntry(String type, String algorithm, int blockSize, int keySize)
 		{
 			this.type = type;
+			this.algorithm = algorithm;
 			this.blocksize = blockSize;
 			this.keysize = keySize;
-			this.cipherClass = cipherClass;
 		}
 	}
 
-	static Vector ciphers = new Vector();
+	static Vector<CipherEntry> ciphers = new Vector<>();
 
 	static
 	{
 		/* Higher Priority First */
 
-		ciphers.addElement(new CipherEntry("aes256-ctr", 16, 32, "com.trilead.ssh2.crypto.cipher.AES"));
-		ciphers.addElement(new CipherEntry("aes192-ctr", 16, 24, "com.trilead.ssh2.crypto.cipher.AES"));
-		ciphers.addElement(new CipherEntry("aes128-ctr", 16, 16, "com.trilead.ssh2.crypto.cipher.AES"));
-		ciphers.addElement(new CipherEntry("blowfish-ctr", 8, 16, "com.trilead.ssh2.crypto.cipher.BlowFish"));
+		ciphers.addElement(new CipherEntry("aes256-ctr", "AES/CTR/NoPadding", 16, 32));
+		ciphers.addElement(new CipherEntry("aes192-ctr", "AES/CTR/NoPadding", 16, 24));
+		ciphers.addElement(new CipherEntry("aes128-ctr", "AES/CTR/NoPadding", 16, 16));
+		ciphers.addElement(new CipherEntry("blowfish-ctr", "Blowfish/CTR/NoPadding", 8, 16));
 
-		ciphers.addElement(new CipherEntry("aes256-cbc", 16, 32, "com.trilead.ssh2.crypto.cipher.AES"));
-		ciphers.addElement(new CipherEntry("aes192-cbc", 16, 24, "com.trilead.ssh2.crypto.cipher.AES"));
-		ciphers.addElement(new CipherEntry("aes128-cbc", 16, 16, "com.trilead.ssh2.crypto.cipher.AES"));
-		ciphers.addElement(new CipherEntry("blowfish-cbc", 8, 16, "com.trilead.ssh2.crypto.cipher.BlowFish"));
+		ciphers.addElement(new CipherEntry("aes256-cbc", "AES/CBC/NoPadding", 16, 32));
+		ciphers.addElement(new CipherEntry("aes192-cbc", "AES/CBC/NoPadding", 16, 24));
+		ciphers.addElement(new CipherEntry("aes128-cbc", "AES/CBC/NoPadding", 16, 16));
+		ciphers.addElement(new CipherEntry("blowfish-cbc", "Blowfish/CBC/NoPadding", 8, 16));
 		
-		ciphers.addElement(new CipherEntry("3des-ctr", 8, 24, "com.trilead.ssh2.crypto.cipher.DESede"));
-		ciphers.addElement(new CipherEntry("3des-cbc", 8, 24, "com.trilead.ssh2.crypto.cipher.DESede"));
+		ciphers.addElement(new CipherEntry("3des-ctr", "DESede/CTR/NoPadding", 8, 24));
+		ciphers.addElement(new CipherEntry("3des-cbc", "DESede/CBC/NoPadding", 8, 24));
 	}
 
 	public static String[] getDefaultCipherList()
 	{
-		String list[] = new String[ciphers.size()];
+		String[] list = new String[ciphers.size()];
 		for (int i = 0; i < ciphers.size(); i++)
 		{
-			CipherEntry ce = (CipherEntry) ciphers.elementAt(i);
-			list[i] = new String(ce.type);
+			CipherEntry ce = ciphers.elementAt(i);
+			list[i] = ce.type;
 		}
 		return list;
 	}
@@ -66,35 +67,17 @@ public class BlockCipherFactory
 
 	public static BlockCipher createCipher(String type, boolean encrypt, byte[] key, byte[] iv)
 	{
-		try
-		{
-			CipherEntry ce = getEntry(type);
-			Class cc = Class.forName(ce.cipherClass);
-			BlockCipher bc = (BlockCipher) cc.newInstance();
-
-			if (type.endsWith("-cbc"))
-			{
-				bc.init(encrypt, key);
-				return new CBCMode(bc, iv, encrypt);
-			}
-			else if (type.endsWith("-ctr"))
-			{
-				bc.init(true, key);
-				return new CTRMode(bc, iv, encrypt);
-			}
-			throw new IllegalArgumentException("Cannot instantiate " + type);
-		}
-		catch (Exception e)
-		{
-			throw new IllegalArgumentException("Cannot instantiate " + type);
-		}
+		CipherEntry ce = getEntry(type);
+		BlockCipher bc = JreCipherWrapper.getInstance(ce.algorithm, new IvParameterSpec(iv));
+		bc.init(encrypt, key);
+		return bc;
 	}
 
 	private static CipherEntry getEntry(String type)
 	{
 		for (int i = 0; i < ciphers.size(); i++)
 		{
-			CipherEntry ce = (CipherEntry) ciphers.elementAt(i);
+			CipherEntry ce = ciphers.elementAt(i);
 			if (ce.type.equals(type))
 				return ce;
 		}
