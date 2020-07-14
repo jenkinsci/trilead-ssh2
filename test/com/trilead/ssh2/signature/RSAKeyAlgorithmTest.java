480bf7a2056a9455031f869a89cd2bfc04a73867
Matt Sicker <boards@gmail.com>
2020-07-14 15:24:29 -0500

    [JENKINS-62311] Add support for RFC 8332 (#47)
    This adds support for the two new key algorithms specified in RFC 8332: RSA with SHA-256 and SHA-512. These still use the same key format as the SHA-1 variant (ssh-rsa), though the signature names are updated to rsa-sha2-256 and rsa-sha2-512.
    
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- test/com/trilead/ssh2/signature/RSAKeyAlgorithmTest.java
+++ test/com/trilead/ssh2/signature/RSAKeyAlgorithmTest.java
@@ -3,6 +3,8 @@ package com.trilead.ssh2.signature;
 import com.trilead.ssh2.crypto.PEMDecoder;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -12,6 +14,8 @@ import java.security.KeyPairGenerator;
 import java.security.SecureRandom;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -21,11 +25,26 @@ import static org.junit.Assert.assertTrue;
 /**
  * @author Michael Clarke
  */
+@RunWith(Parameterized.class)
 public class RSAKeyAlgorithmTest {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+                new Object[]{"SHA1WithRSA", "ssh-rsa"},
+                new Object[]{"SHA256withRSA", "rsa-sha2-256"},
+                new Object[]{"SHA512withRSA", "rsa-sha2-512"}
+        );
+    }
+
+    private final RSAKeyAlgorithm testCase;
+
+    public RSAKeyAlgorithmTest(String signatureAlgorithm, String keyFormat) {
+        testCase = new RSAKeyAlgorithm(signatureAlgorithm, keyFormat);
+    }
 
     @Test
     public void testEncodeDecodePublicKey() throws GeneralSecurityException, IOException {
-        RSAKeyAlgorithm testCase = new RSAKeyAlgorithm();
         KeyPairGenerator factory = KeyPairGenerator.getInstance("RSA");
         RSAPublicKey publicKey = (RSAPublicKey) factory.generateKeyPair().getPublic();
         byte[] encoded = testCase.encodePublicKey(publicKey);
@@ -35,7 +54,6 @@ public class RSAKeyAlgorithmTest {
 
     @Test
     public void testEncodeDecodeSignature() throws GeneralSecurityException, IOException {
-        RSAKeyAlgorithm testCase = new RSAKeyAlgorithm();
         KeyPairGenerator factory = KeyPairGenerator.getInstance("RSA");
         RSAPrivateKey privateKey = (RSAPrivateKey) factory.generateKeyPair().getPrivate();
         byte[] signature = testCase.generateSignature("Sign Me".getBytes(StandardCharsets.UTF_8), privateKey, new SecureRandom());
@@ -46,7 +64,6 @@ public class RSAKeyAlgorithmTest {
 
     @Test
     public void testSignAndVerify() throws GeneralSecurityException, IOException {
-        RSAKeyAlgorithm testCase = new RSAKeyAlgorithm();
         byte[] message = "Signature Test".getBytes(StandardCharsets.UTF_8);
         KeyPairGenerator factory = KeyPairGenerator.getInstance("RSA");
         KeyPair keyPair = factory.generateKeyPair();
@@ -59,7 +76,6 @@ public class RSAKeyAlgorithmTest {
 
     @Test
     public void testSignAndVerifyFailure() throws GeneralSecurityException, IOException {
-        RSAKeyAlgorithm testCase = new RSAKeyAlgorithm();
         byte[] message = "Signature Test 2".getBytes(StandardCharsets.UTF_8);
         KeyPairGenerator factory = KeyPairGenerator.getInstance("RSA");
         KeyPair keyPair = factory.generateKeyPair();
