e952a05eb3e7e3377854ffd11f31bc592c90db75
SlipkHunter <abrito025@gmail.com>
2022-01-13 15:58:30 -0300

    Corrigido erro que travava na autenticação, o timeout estava alto demais

--- src/main/java/com/trilead/ssh2/auth/AuthenticationManager.java
+++ src/main/java/com/trilead/ssh2/auth/AuthenticationManager.java
@@ -30,11 +30,10 @@ import java.util.stream.Collectors;
 public class AuthenticationManager implements MessageHandler
 {
 	public static final String PROPERTY_TIMEOUT = AuthenticationManager.class.getName() + ".timeout";
-	public static final long TIMEOUT = Long.valueOf(System.getProperty(PROPERTY_TIMEOUT,"120000"));
+	public static final long TIMEOUT = Long.valueOf(System.getProperty(PROPERTY_TIMEOUT, "10000"));//"120000"));
 	TransportManager tm;
 
 	private Vector<ConnectionMonitor> connectionMonitors;
-	private boolean monitorsWereInformed = false;
 
 	Vector packets = new Vector();
 	boolean connectionClosed = false;
