08429d344199daf5a840251a693649a8d570106a
mpet <mikaelpetterson1@gmail.com>
2023-08-16 13:55:06 +0200

    Update Connection.java

--- src/com/trilead/ssh2/Connection.java
+++ src/com/trilead/ssh2/Connection.java
@@ -665,7 +665,7 @@ public class Connection
 		if (kexTimeout < 0)
 			throw new IllegalArgumentException("kexTimeout must be non-negative!");
 
-		final TimeoutState state = new TimeoutState():		    
+		final TimeoutState state = new TimeoutState();		    
 		final TimeoutService timeoutService = new TimeoutService(hostname);		    
 
 		tm = new TransportManager(hostname, port, sourceAddress);
